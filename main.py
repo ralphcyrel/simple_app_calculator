67cadb5dcb0da61e4d08f4e09d2522e57392ffcd
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-05-09 10:16:58 +0800

    the program that will work the operator

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ while True:
 #This will ask the user to input his/her number
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
+#This will run the program in choose if the operator
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 * num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        result = num1 / num2
 #This will display the result
 #Will ask the user if they want to try again
 #Exit the program if the user doesn't want to try again
