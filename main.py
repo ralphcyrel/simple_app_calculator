3a692c419deca06febaff579b27d4c74157058f4
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-05-09 10:11:16 +0800

    Added pseudocode

---
+++ main.py
@@ -0,0 +1,9 @@
+#Ralph Cyrel B. Ronda
+#BSCPE 1-2
+#Program #5: Simple App Calculator
+
+#This will ask the user to input his/her operator
+#This will ask the user to input his/her number
+#This will display the result
+#Will ask the user if they want to try again
+#Exit the program if the user doesn't want to try again
