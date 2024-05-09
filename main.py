8fedcb79dc77fc9720fe737be8e7da0677dfd634
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-05-09 10:15:10 +0800

    user input

--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 #BSCPE 1-2
 #Program #5: Simple App Calculator
 
+while True:
 #This will ask the user to input his/her operator
+    operator = input("Please enter an operator (+, -, *, /):")
 #This will ask the user to input his/her number
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
 #This will display the result
 #Will ask the user if they want to try again
 #Exit the program if the user doesn't want to try again
